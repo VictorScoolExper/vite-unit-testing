1fb978a01b950f322cd75d23b39837cce789a2b9
VictorScoolExper <victorscool477@outlook.com>
2023-07-15 20:23:11 -0600

    added tests

--- basic-testing-frontend/src/math.js
+++ basic-testing-frontend/src/math.js
@@ -2,7 +2,9 @@ export function add(numbers) {
   let sum = 0;
 
   for (const number of numbers) {
-    sum += number;
+    // Added the "+" in front of the variable 
+    // returns a NaN if not valid or a number if valid
+    sum += +number;
   }
   return sum;
 }
