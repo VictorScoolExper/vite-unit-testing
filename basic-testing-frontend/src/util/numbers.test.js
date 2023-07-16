198c1154a14b83d76fb2a7729164e5f2f097c616
VictorScoolExper <victorscool477@outlook.com>
2023-07-16 14:15:11 -0600

    finished section

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,31 +1,54 @@
-import { it, expect } from "vitest";
+import { it, expect, describe } from "vitest";
 
-import { transformToNumber } from "./numbers";
+import { cleanNumbers, transformToNumber } from "./numbers";
 
-it("should transform a string number to a number of type number", () => {
-  const input = "1";
+describe("transformToNumber()", () => {
+  it("should transform a string number to a number of type number", () => {
+    const input = "1";
 
-  const result = transformToNumber(input);
+    const result = transformToNumber(input);
 
-  expect(result).toBeTypeOf("number");
+    expect(result).toBeTypeOf("number");
+  });
+
+  // Note that it is not always correct to have duplicate test or test that are very similiar
+  it("should transform a string number to a number of type number", () => {
+    const input = "1";
+
+    const result = transformToNumber(input);
+
+    expect(result).toBe(+input);
+  });
+
+  it("should yield NaN for non-transformable values", () => {
+    const input = "invalid";
+    const input2 = {};
+
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+
+    expect(result).toBeNaN();
+    expect(result2).toBeNaN();
+  });
+});
+// integrated tests
+describe("cleanNumbers()", () => {
+  it("should return an array of number values if an array of string number values is provided", () => {
+    const numberValues = ["1", "2"];
+
+    const cleanedNumbers = cleanNumbers(numberValues);
+
+    expect(cleanedNumbers[0]).toBeTypeOf("number");
+  });
+  
+  // test that functions actually perform correct in integrated functions
+  it("should throw an error if an array with at least one empty string is provided", () => {
+    const numberValues = ['', 1];
+
+    const cleanFn = () => cleanNumbers(numberValues);
+
+    expect(cleanFn).toThrow();
+  })
 });
 
-// Note that it is not always correct to have duplicate test or test that are very similiar
-it("should transform a string number to a number of type number", () => {
-  const input = "1";
 
-  const result = transformToNumber(input);
-
-  expect(result).toBe(+input);
-});
-
-it("should yield NaN for non-transformable values", () => {
-  const input = "invalid";
-  const input2 = {};
-
-  const result = transformToNumber(input);
-  const result2 = transformToNumber(input2);
-
-  expect(result).toBeNaN();
-  expect(result2).toBeNaN();
-});
