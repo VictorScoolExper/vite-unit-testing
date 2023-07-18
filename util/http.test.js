86e54a18aa7eedb6b46bed71d7609faec9473f9d
VictorScoolExper <victorscool477@outlook.com>
2023-07-18 17:44:05 -0600

    finished section

---
+++ util/http.test.js
@@ -0,0 +1,65 @@
+import { it, expect, vi } from "vitest";
+import { sendDataRequest } from "./http";
+import { HttpError } from "./errors";
+
+const testResponseData = {testKey: 'testData'};
+const testFetch = vi.fn((url, options) => {
+    return new Promise((resolve, reject) => {
+        if(typeof options.body !== 'string'){
+            return reject('Not a string.');
+        }
+        const testResponse = {
+            ok: true,
+            json() {
+                return new Promise((resolve, reject) => {
+                    resolve(testResponseData);
+                })
+            }
+        };
+        resolve(testResponse);
+    })
+});
+
+vi.stubGlobal('fetch', testFetch);
+
+it('should return any available response data', () => {
+    const testData = {key: 'test'};
+    
+    return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+});
+
+it('should convert the provided data to JSON before sending the request', async () => {
+    const testData = {key: 'test'};
+
+    let errorMessage;
+
+    try {
+        await expect(sendDataRequest(testData));
+    } catch (error) {
+        errorMessage = error;
+    }
+
+    expect(errorMessage).not.toBe('Not a string');
+});
+// Replacement for fetch function functionality
+it('should throw an HttpError in case of non-ok responses', () => {
+    // We create a replacement function for Fetch
+    testFetch.mockImplementationOnce((url, options) => {
+        return new Promise((resolve, reject) => {
+            const testResponse = {
+                ok: false,
+                json() {
+                    return new Promise((resolve, reject) => {
+                        resolve(testResponseData);
+                    })
+                }
+            };
+            resolve(testResponse);
+        })
+    });
+
+    const testData = 'test';
+
+    // checks if error is instance of HttpError 
+    return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError);
+});
