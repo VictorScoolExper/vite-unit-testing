1018c5ab9ac83273bb2ee7d498854595722a774a
VictorScoolExper <victorscool477@outlook.com>
2023-07-19 11:18:12 -0600

    finished section

---
+++ util/dom.test.js
@@ -0,0 +1,50 @@
+// we can use these nodejs api
+import fs from "fs";
+import path from "path";
+
+import { it, expect, vi, beforeEach } from "vitest";
+import { Window } from "happy-dom";
+
+// Both vtest and jest emulate/testing dom called Happy-Dom
+// Note that in jest it would be called different
+
+import { showError } from "./dom";
+
+// we initialize our dom with
+const htmlDocPath = path.join(process.cwd(), "index.html");
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
+
+const window = new Window();
+const document = window.document;
+vi.stubGlobal("document", document);
+
+beforeEach(() => {
+  document.body.innerHTML = "";
+  document.write(htmlDocumentContent);
+});
+
+it('should add an error paragraph to the id="errors" element', () => {
+  showError("Test");
+
+  const errorsEl = document.getElementById("errors");
+  const errorParagraph = errorsEl.firstElementChild;
+
+  expect(errorParagraph).not.toBeNull();
+});
+
+it("should not contain an error paragraph initially", () => {
+  const errorsEl = document.getElementById("errors");
+  const errorParagraph = errorsEl.firstElementChild;
+
+  expect(errorParagraph).toBeNull();
+});
+
+it("should output the provided message in the error paragraph", () => {
+  const testErrorMessage = "Test";
+  showError(testErrorMessage);
+
+  const errorsEl = document.getElementById("errors");
+  const errorParagraph = errorsEl.firstElementChild;
+
+  expect(errorParagraph.textContent).toBe(testErrorMessage);
+});
